4cf1393d3429416c8d212180ab215b1f1e7802b6
Fatou Amar <amar.fatou@gmail.com>
2025-07-19 22:56:20 +0200

    Initialisation du portfolio - projet P12

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # P12-testeur-logiciel-portfolio
+
+# Portfolio - Fatou AMAR
+
+Bienvenue sur le dépôt GitHub de mon portfolio professionnel en ligne.  
+Ce site présente mes compétences, mes projets et mon parcours en tant qu'ingénieure Test Logiciel.
+
+## Lien vers le site en ligne
+
+[Accéder au portfolio en ligne](https://fadichou.github.io/portfolio)
+
+## Technologies utilisées
+
+- HTML5
+- CSS3
+- JavaScript
+- Bootstrap
+- Git / GitHub
+- Outils d'audit accessibilité :WAVE
+
+## Fonctionnalités
+
+- Section **À propos** présentant mon profil
+- Section **Compétences** dynamique, alimentée par un fichier 'skills.json'
+- Section **Projets** dynamique, alimentée par un fichier 'portfolio.json'
+- Section **Contactez-mois** pour me contacter via le formulaire
+
+  Chaque projet contient :
+
+  - une image,
+  - une description,
+  - un lien vers le code source
+
+- Formulaire de contact avec message de confirmation
+
+## Optimisations et corrections
+
+- ✅ Correction de l'affichage des icônes Font Awesome
+- ✅ Correction de l'affichage des éléments dynamiques
+- ✅ Résolution d'erreurs d’accessibilité (contrastes, titres, attributs `alt`)
+- ✅ Optimisation des images (taille, format)
+- ✅ Structure des titres respectant la hiérarchie sémantique
+
+## Personnalisation
+
+- Mise en avant de mes projets OpenClassrooms
+- Présentation claire de mon rôle sur chaque projet
+- Visuel et textes personnalisés
+
+## Lancer le projet en local
+
+```bash
+git clone https://github.com/fadichou/portfolio.git
+cd portfolio
+# Ouvrir index.html dans un navigateur
+```
--- script/script.js
+++ script/script.js
@@ -1,102 +1,109 @@
 // Function to add the "navbarDark" class to the navbar on scroll
 function handleNavbarScroll() {
-    const header = document.querySelector(".navbar");
-    window.onscroll = function () {
-        const top = window.scrollY;
-        if (top >= 100) {
-            header.classList.add("navbarDark");
-        } else {
-            header.classList.remove("navbarDark");
-        }
-    };
+  const header = document.querySelector(".navbar");
+  window.onscroll = function () {
+    const top = window.scrollY;
+    if (top >= 100) {
+      header.classList.add("navbarDark");
+    } else {
+      header.classList.remove("navbarDark");
+    }
+  };
 }
 
 // Function to handle navbar collapse on small devices after a click
 function handleNavbarCollapse() {
-    const navLinks = document.querySelectorAll(".nav-item");
-    const menuToggle = document.getElementById("navbarSupportedContent");
+  const navLinks = document.querySelectorAll(".nav-item");
+  const menuToggle = document.getElementById("navbarSupportedContent");
 
-    navLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            new bootstrap.Collapse(menuToggle).toggle();
-        });
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      new bootstrap.Collapse(menuToggle).toggle();
     });
+  });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createSkillsFromJSON() {
-    const container = document.querySelector("#skills .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#skills .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    // Load the JSON file
-    fetch("data/skills.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
+  // Load the JSON file
+  fetch("data/skills.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./images/${item.image}" />
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                             <img src="./images/${item.image}" alt="${item.alt}" />
+                            <h3 class="card-title mt-3">${item.title}</h3>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
                 `;
 
-                // Append the card to the current row
-                row.appendChild(card);
+        // Append the card to the current row
+        row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
 // Function to dynamically create HTML elements from the JSON file
 function createPortfolioFromJSON() {
-    const container = document.querySelector("#portfolio .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#portfolio .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    // Load the JSON file
-    fetch("data/portfolio.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
+  // Load the JSON file
+  fetch("data/portfolio.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" style="width:100%">
+                    <img class="card-img-top" src="images/${item.image}" alt="${
+          item.alt
+        }" style="width:100%">
                     <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
+                        <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                            <a href="${item.link}" 
+         target="${item.target || "_self"}" 
+         rel="${item.target === "_blank" ? "noopener noreferrer" : ""}" 
+         class="btn btn-success">
+         Lien
+      </a>
                         </div>
                     </div>
                 </div>
                 `;
 
-                // Append the card to the current row
-                row.apendChild(card);
+        // Append the card to the current row
+        row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
 
 // Call the functions to execute the code
@@ -104,3 +111,43 @@ handleNavbarScroll();
 handleNavbarCollapse();
 createSkillsFromJSON();
 createPortfolioFromJSON();
+//handleNavbarScroll();//
+//handleNavbarCollapse();//
+//createSkillsFromJSON();//
+//createPortfolioFromJSON();//
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+  const confirmationMessage = document.getElementById("confirmationMessage");
+
+  if (!form) return;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          confirmationMessage.classList.remove("d-none");
+          form.reset();
+
+          setTimeout(() => {
+            confirmationMessage.classList.add("d-none");
+          }, 5000);
+        } else {
+          alert("Erreur lors de l'envoi du message. Veuillez réessayer.");
+        }
+      })
+      .catch((error) => {
+        alert("Erreur de réseau. Vérifiez votre connexion.");
+      });
+  });
+});
